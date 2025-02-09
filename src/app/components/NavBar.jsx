a4def9a2597edf747662036fe52f273a3e5fa8b4
kausar017 <mdkousarmia71@gmail.com>
2025-02-09 12:07:49 +0600

    card disign impliment

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -2,22 +2,73 @@ import Link from "next/link";
 import React from "react";
 
 export default function NavBar() {
+  const link = (
+    <>
+      <Link href={"/"} className="btn btn-sm btn-ghost">
+        <li>Home</li>
+      </Link>
+      <Link href={"/services"} className="btn btn-sm btn-ghost">
+        <li>Services</li>
+      </Link>
+      <Link href={"/about"} className="btn btn-sm btn-ghost">
+        <li>About</li>
+      </Link>
+      <Link href={"/contact"} className="btn btn-sm btn-ghost">
+        <li>Contact</li>
+      </Link>
+    </>
+  );
+
   return (
-    <nav className=" bg-gray-400">
-      <ul className="flex justify-center items-center container mx-auto h-full min-h-12 gap-x-8">
-        <Link href={"/"}>
-          <li>Home</li>
-        </Link>
-        <Link href={"/services"}>
-          <li>Services</li>
-        </Link>
-        <Link href={"/about"}>
-          <li>About</li>
-        </Link>
-        <Link href={'/contact'}>
-          <li>Contact</li>
-        </Link>
-      </ul>
-    </nav>
+    <>
+      <div className="bg-base-200">
+        <div className="navbar container mx-auto">
+          <div className="navbar-start ">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              {
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                >
+                  {link}
+                </ul>
+              }
+            </div>
+            <a className="btn btn-ghost text-xl">Bloge Service</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{link}</ul>
+          </div>
+          <div className="navbar-end">
+            <Link
+              href={"/singIn"}
+              className="btn btn-sm btn-outline btn-primary"
+            >
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -21,25 +21,36 @@ export default function Services() {
 
   return (
     <div className="container mx-auto">
-      <h1>Services</h1>
-      <div className="grid grid-cols-4 gap-5">
-        {services.map((service) => (
-          <div key={service?.id} className="card card-compact bg-base-100 shadow-xl">
-            <figure>
-              <img
-              className="h-80 w-full"
-                src={service?.download_url}
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <div className="card-actions justify-end">
-                <Link href={`services/${service?.id}`} className="btn btn-sm btn-outline btn-primary">Buy Now</Link>
+      <h1 className="text-center text-4xl font-bold pt-5">
+        Services <span className="divider"></span>
+      </h1>
+      {services.length == 0 ? (
+        <div className="flex flex-col justify-center items-center min-h-[800px]">
+          <span className="loading loading-ring w-24 "></span>
+        </div>
+      ) : (
+        <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-5 px-2">
+          {services.map((service) => (
+            <div
+              key={service?.id}
+              className="card card-compact bg-base-100 shadow-xl overflow-hidden group"
+            >
+              <figure className="relative">
+                <img
+                  className="h-80 w-full"
+                  src={service?.download_url}
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="absolute py-6 bg-black bg-opacity-40 w-full text-center -top-20 group-hover:top-0 transition-all duration-300">
+                <Link href={`services/${service?.id}`} className="">
+                  <span className=" text-center text-white text-2xl hover:text-red-500"> Detals</span>
+                </Link>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
